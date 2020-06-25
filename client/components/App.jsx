f0cefad2acf161d2e92cf943d623039761d098c1
Richard E <46254658+richard-eman@users.noreply.github.com>
2020-06-25 19:54:54 +1200

    Create CombatSystem.jsx as a class with a button

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Form from './Form'
 import Arena from './Arena'
+import CombatSystem from './CombatSystem'
 
 class App extends React.Component {
 
@@ -17,12 +18,18 @@ componentDidUpdate() {
   console.log(this.state);
 }
 
+// get random enemy function:
+// This should be done after form page, ya?
+// For arena:
+// get ai data: name, front-sprite,
+
   render() {
     return (
       <>
       <h1>Pokemon</h1>
       <Form setName={this.setName}/>
       <Arena/>
+      <CombatSystem/>
       </>
     )
   }
---
+++ client/components/CombatSystem.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+class CombatSystem extends React.Component {
+
+  state = {
+    userHP : 100,
+    aiHP : 100
+  }
+
+  render(){
+    return(
+      <button>yo</button>
+    )
+  }
+}
+
+export default CombatSystem
+
+// basic combat system component:
+
+//o ai and player will have state hp of 100 on mount
+// attack button
+// on button click, a player attack number and ai attack number between between 10 and 20 will be randomly generated.
+// these numbers will be set in state
+// health will have the attack values generated subtracted.
+// once ai or player reaches less than zero health, win screen should be loaded (stretch)
+
+
+// combat animations stretch:
+
+// in combat system component:
+// onclick, a function that was imported from App will be run so that a css animation will play in arena component.
