5ac87e8dcc21734d4cf44053b862c800d4435695
JaydenHall <jaydenhallpt@hotmail.co.nz>
2020-06-25 18:38:45 +1200

    initial commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Form from './Form'
 
-const App = () => {
-  return (
-    <h1>Pokemon</h1>
-  )
+class App extends React.Component {
+
+  state = {
+    name: '',
+    pokemonName: ''
+  }
+
+setName = (name, pokemonName) => {
+  this.setState({name: name, pokemonName: pokemonName})
 }
+
+componentDidUpdate() {
+  console.log(this.state);
+}
+
+  render() {
+    return (
+      <>
+      <h1>Pokemon</h1>
+      <Form setName={this.setName}/>
+      </>
+    )
+  }
+  }
 
 export default App
