f4b2560c34d30c3647d053969cd4a194609c9104
Nick-Jarvie <nickjarvie@gmail.com>
2020-06-26 10:43:13 +1200

    update css and button

--- client/components/Arena.jsx
+++ client/components/Arena.jsx
@@ -5,12 +5,16 @@ import request from 'superagent'
 class Arena extends React.Component {
   state = {
     userSprite: '',
+    userPokemonName: '',
     aiSprite: '',
+    aiPokemonName: '',
   }
 
   componentDidMount() {
     this.getMyPokemon()
+    this.getMyPokemonName()
     this.getAiPokemon()
+    this.getAiPokemonName()
   }
 
   // handleClick = () => {
@@ -27,6 +31,16 @@ class Arena extends React.Component {
       })
   }
 
+  getMyPokemonName = () => {
+    // take input from form and put at end of URL
+    request.get('https://pokeapi.co/api/v2/pokemon/1/')
+      .then(res => {
+        this.setState({
+          userPokemonName: res.body.name,
+        })
+      })
+  }
+
   getAiPokemon = () => {
     // randomPokemonFunction outputs number to end of URL
     request.get('https://pokeapi.co/api/v2/pokemon/1/')
@@ -37,13 +51,29 @@ class Arena extends React.Component {
       })
   }
 
+  getAiPokemonName = () => {
+    // take input from form and put at end of URL
+    request.get('https://pokeapi.co/api/v2/pokemon/1/')
+      .then(res => {
+        this.setState({
+          aiPokemonName: res.body.name,
+        })
+      })
+  }
+
   render() {
     return (
       <>
       <div>
         <h1>This is the arena for fighting pokemon</h1>
-        <img src={this.state.userSprite} alt="userSprite"/>
-        <img src={this.state.aiSprite} alt="aiSprite"/>
+        <div className='userPokemon'>
+          <img className='userPokemonSprite' src={this.state.userSprite} alt="userSprite"/>
+          <p>{this.state.userPokemonName}</p>
+        </div>
+        <div classname='aiPokemon'>
+          <img className='aiPokemonSprite' src={this.state.aiSprite} alt="aiSprite"/>
+          <p>{this.state.aiPokemonName}</p>
+        </div>
       </div>
       </>
     )
