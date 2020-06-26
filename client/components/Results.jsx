b575c8341141f797773245b671b327461a24b50e
Rose-Mead <rmkmead@hotmail.com>
2020-06-26 15:13:57 +1200

    Add results table

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -1,13 +1,68 @@
 import React from 'react'
 import HomeButton from "./HomeButton"
+import { getUsers } from '../api/index'
+import { get } from 'superagent'
 
-const Results = () => {
-  return (
-    <React.Fragment>
-      <HomeButton />
-      <h1>This is the results (stretch)</h1>
-    </React.Fragment>
-  )
+class Results extends React.Component {
+
+  state = {
+    users: {},
+    lis: []
+  }
+
+  componentDidMount() {
+    getUsers()
+      .then(users => {
+        this.setState({
+          users: users
+        })
+      })
+      .then(() =>{
+        this.setState({
+          lis: this.makeResultsTable()
+        })
+
+      })
+ 
+  }
+  
+  makeResultsList() {
+    return this.state.users.map(user => {
+        return<li>{user.name} {user.wins} {user.losses}</li>
+          })
+  }
+
+  makeResultsTable() {
+    return this.state.users.map(user => {
+        return<tr>
+        <td>{user.name}</td>
+        <td>{user.wins}</td>
+        <td>{user.losses}</td>
+        </tr>
+          })
+  }
+
+
+
+
+  render() {
+    return (
+      <React.Fragment >
+        <HomeButton />
+        <h1>This is the results (stretch)</h1>
+        <table>
+          <tr>
+            <td>Name</td>
+            <td>Wins</td>
+            <td>Losses</td>
+          </tr>
+        {this.state.lis}
+          
+        </table>
+      </React.Fragment >
+    )
+  }
+
 }
 
 export default Results
